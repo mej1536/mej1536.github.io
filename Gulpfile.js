654a18d167bdd569dfe992e2a1b81d54113f7798
은정 <@192.168.0.109>
2017-02-18 17:14:52 +0900

    줄바꿈 \r\n/ 변경

--- Gulpfile.js
+++ Gulpfile.js
@@ -101,8 +101,8 @@ gulp.task('styleSASS',function(){
 			cascade: false
 		}))
 		.pipe(cssComb('csscomb.json'))
-		.pipe(replace('/*! -----','\n/*! -----'))
-		.pipe(replace(';}}',';}\n}'))
+		.pipe(replace('/*! -----','\r\n/*! -----'))
+		.pipe(replace(';}}',';}\r\n}'))
 		.pipe(gulp.dest(_sassDEST))
 		.pipe(gcallback(function(){
 				console.log('** task done : styleSASS');
